3308d086b6eb24ecd9e329f6458ab8a3696f9913
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-09-06 10:11:03 +0900

    ss

--- Game/00_SSAGame/Elsa20200001/Elsa20200001/Commons/SCommon.cs
+++ Game/00_SSAGame/Elsa20200001/Elsa20200001/Commons/SCommon.cs
@@ -694,6 +694,22 @@ namespace Charlotte.Commons
 				File.Copy(file, Path.Combine(wDir, Path.GetFileName(file)));
 		}
 
+		public static void CopyPath(string rPath, string wPath)
+		{
+			if (Directory.Exists(rPath))
+			{
+				SCommon.CopyDir(rPath, wPath);
+			}
+			else if (File.Exists(rPath))
+			{
+				File.Copy(rPath, wPath);
+			}
+			else
+			{
+				throw new Exception("コピー元パスが存在しません。");
+			}
+		}
+
 		public static string EraseExt(string path)
 		{
 			return Path.Combine(Path.GetDirectoryName(path), Path.GetFileNameWithoutExtension(path));
@@ -2004,9 +2020,13 @@ namespace Charlotte.Commons
 
 		public static void Batch(IList<string> commands, string workingDir = "", StartProcessWindowStyle_e winStyle = StartProcessWindowStyle_e.INVISIBLE)
 		{
+			// Batch-名は何でも良い。
+			// 折角なので何かの時のためにタスマネから目視で発見・判別し易い名前にしておく。
+			const string BATCH_NAME = "ChocolateCupCakeRecipe.bat";
+
 			using (WorkingDir wd = new WorkingDir())
 			{
-				string batFile = wd.GetPath("a.bat");
+				string batFile = wd.GetPath(BATCH_NAME);
 
 				File.WriteAllLines(batFile, commands, ENCODING_SJIS);
 
@@ -2103,6 +2123,9 @@ namespace Charlotte.Commons
 				this.Chars = (SCommon.ALPHA + SCommon.alpha + SCommon.DECIMAL + "+/").ToArray();
 				this.CharMap = new byte[(int)char.MaxValue + 1];
 
+				for (int index = 0; index <= (int)char.MaxValue; index++)
+					this.CharMap[index] = 0xff;
+
 				for (int index = 0; index < 64; index++)
 					this.CharMap[this.Chars[index]] = (byte)index;
 
@@ -2172,29 +2195,20 @@ namespace Charlotte.Commons
 			/// <summary>
 			/// Base64を元のバイト列に変換します。
 			/// 対応フォーマット：
-			/// -- Base64 Encode -- 但し改行を含まないこと。パディング無しでも良い。
+			/// -- Base64 Encode -- パディング無しでも良い。余計な空白・改行が含まれていても良い。
 			/// -- Base64 URL Encode
 			/// </summary>
 			/// <param name="src">Base64</param>
 			/// <returns>元のバイト列</returns>
 			public byte[] Decode(string src)
 			{
-				while (src.Length % 4 != 0)
-					src += CHAR_PADDING;
-
-				int destSize = (src.Length / 4) * 3;
-
-				if (destSize != 0)
+				// パディング除去
+				// 空白・改行などの不要な文字を除去する。
 				{
-					if (src[src.Length - 2] == CHAR_PADDING)
-					{
-						destSize -= 2;
-					}
-					else if (src[src.Length - 1] == CHAR_PADDING)
-					{
-						destSize--;
-					}
+					src = new string(src.Where(v => this.CharMap[(int)v] != 0xff).ToArray());
 				}
+
+				int destSize = (int)(((long)src.Length * 3) / 4);
 				byte[] dest = new byte[destSize];
 				int writer = 0;
 				int index = 0;
@@ -2647,11 +2661,15 @@ namespace Charlotte.Commons
 
 		public static void ToThrowPrint(Action routine)
 		{
-			Console.WriteLine(ToThrow(routine));
-			Console.WriteLine("★★★想定された例外のため処理を続行します。");
+			Console.WriteLine("想定された例外：" + ToThrow(routine).Message);
 		}
 
 		#region GetOutputDir
+
+		// memo:
+		// 慣習的に C:\1, C:\2, C:\3, ... C:\999 をテンポラリ・暗黙の出力先として使用している。
+		// https://github.com/stackprobe/Factory/blob/master/DevTools/zz.bat -- 定期的に全削除する運用 -- 際限なく溜まらない想定
+		// あくまで個人的な慣習なので、使用する際は注意すること。
 
 		private static string GOD_Dir;
 
