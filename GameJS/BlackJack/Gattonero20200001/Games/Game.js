8075ca607973db5fc8436c048032fe9f5d26832c
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-09-24 00:30:55 +0900

    ss

---
+++ GameJS/BlackJack/Gattonero20200001/Games/Game.js
@@ -0,0 +1,25 @@
+/*
+	ÉQÅ[ÉÄÅEÉÅÉCÉì
+*/
+
+function* <generatorForTask> GameMain()
+{
+	FreezeInput();
+	ClearAllActor();
+
+	for (; ; )
+	{
+		if (GetInput_A() == 1)
+		{
+			break;
+		}
+
+		SetColor("#000000");
+		PrintRect(0.0, 0.0, Screen_W, Screen_H);
+
+		ExecuteAllActor();
+
+		yield 1;
+	}
+	FreezeInput();
+}
