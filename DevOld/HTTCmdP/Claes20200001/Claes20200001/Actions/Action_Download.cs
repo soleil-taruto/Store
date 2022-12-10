e6193bbf5676e1a4eeedbce3a6d2b40fda556817
soleil.taruto <soleil.taruto@gmail.com>
2022-12-11 06:23:54 +0900

    Voyager 1 is 23700540376.148 kilometers away from the Sun.

--- DevOld/HTTCmdP/Claes20200001/Claes20200001/Actions/Action_Download.cs
+++ DevOld/HTTCmdP/Claes20200001/Claes20200001/Actions/Action_Download.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Charlotte.WebServices;
 using Charlotte.Commons;
+using Charlotte.WebServices;
 
 namespace Charlotte.Actions
 {
@@ -15,15 +15,19 @@ namespace Charlotte.Actions
 			string downloadPath = GetDownloadPath(channel);
 			string downloadUrl = "http://" + host + downloadPath;
 
-			string resText = string.Format(@"
-
+			string resText = string.Format(@"<html>
+<head>
+<title>ダウンロード</title>
+</head>
+<body>
 <center>
-<h1>ファイルをダウンロードするには以下のリンクをクリックして下さい。</h1>
+<h1>ファイルをダウンロードするには下記リンクをクリックして下さい。</h1>
 <hr/>
-<a href=""{0}"">{1}</a>
+<a href=""{0}"" download>{1}</a>
 </center>
-
-".Trim()
+</body>
+</html>
+"
 				, downloadUrl
 				, downloadUrl
 				);
@@ -39,18 +43,22 @@ namespace Charlotte.Actions
 				if (SCommon.EqualsIgnoreCase(pair[0], name))
 					return pair[1];
 
-			throw new Exception("no header: " + name);
+			throw new Exception("no header name: " + name);
 		}
 
 		private static string GetDownloadPath(HTTPServerChannel channel)
 		{
 			string downloadPath = channel.PathQuery;
-			int p = downloadPath.IndexOf('/', 1);
+
+			if (downloadPath[0] != '/')
+				throw new Exception("Bad donwloadPath 1");
+
+			int p = downloadPath.IndexOf('/', 1); // 2番目の'/'
 
 			if (p == -1)
-				throw new Exception("Bad downloadPath");
+				throw new Exception("Bad downloadPath 2");
 
-			downloadPath = downloadPath.Substring(p);
+			downloadPath = downloadPath.Substring(p); // 2番目の'/'より前を除去
 			return downloadPath;
 		}
 	}
